be5229fe587c1920d4268ebc29f91a2b368a02d9
V3LT0 <mgarridozafra9@gmail.com>
2024-12-15 12:25:14 +0100

    Proyecto Instagram

---
+++ posts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from posts.models import Post, Comment
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["user", "created_at", "caption"]
+
+@admin.register(Comment)
+class CommmentAdmin(admin.ModelAdmin):
+    list_display = ["user", "created_at", "text"]
